e7fa6a71c7cad88002c41189b10d8dbd6ae0b60a
bgacias <bgacias@gmail.com>
2016-04-05 11:03:00 +0200

    exemple hello word amb input de text

--- app/vista3/vista3.js
+++ app/vista3/vista3.js
@@ -2,13 +2,13 @@
 
 angular.module('myApp.vista3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/vista3', {
-    templateUrl: 'vista3/vista3.html',
-    controller: 'Vista3Ctrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/vista3', {
+            templateUrl: 'vista3/vista3.html',
+            controller: 'Vista3Ctrl'
+        });
+    }])
 
-.controller('Vista3Ctrl', [function() {
-        //console.log("controler del modul vista3");
-}]);
+    .controller('Vista3Ctrl', function($scope) {
+        $scope.name3="Hello word";
+    });
