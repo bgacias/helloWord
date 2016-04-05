aade9271d504f25328566e8b2c368d34ca56913d
bgacias <bgacias@gmail.com>
2016-04-05 13:59:52 +0200

    exemple directiva

--- app/vista3/vista3.js
+++ app/vista3/vista3.js
@@ -11,4 +11,12 @@ angular.module('myApp.vista3', ['ngRoute'])
 
     .controller('Vista3Ctrl', function($scope) {
         $scope.name3="Hello word";
-    });
+    })
+    //usa la camel case name es a dir:  w3TestDirective  es invocada separant perl tag  <w3-test-directive>
+    .directive('w3TestDirective',function(){
+        return {
+            template: "<p>Això és el resultat retornat  per una directiva</p>"
+        };
+    })
+
+;
